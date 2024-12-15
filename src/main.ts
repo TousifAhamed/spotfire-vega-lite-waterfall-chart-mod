c8488f77bbaf40df6ec1f5dd25c4558bbcfe2672
hski <holger@schimanski-web.de>
2024-12-15 12:39:37 +0100

    Code cleanup

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,20 @@ import * as vega from "vega";
 import * as vl from "vega-lite";
 
 Spotfire.initialize(async (mod: Spotfire.Mod) => {
-    /**
-     * Create the read function.
-     */
+
+    // Create the read function.
     const reader = mod.createReader(mod.visualization.data(), mod.windowSize(), mod.property("myProperty"));
 
-    /**
-     * Store the context.
-     */
+    // Store the context.
     const context = mod.getRenderContext();
 
-    /**
-     * Initiate the read loop
-     */
+    // Initiate the read loop
     reader.subscribe(render);
 
+
     async function render(dataView: Spotfire.DataView, windowSize: Spotfire.Size, prop: Spotfire.ModProperty<string>) {
-        /**
-         * Check the data view for errors
-         */
+
+        // Check the data view for errors
         let errors = await dataView.getErrors();
         if (errors.length > 0) {
             // Showing an error overlay will hide the mod iframe.
@@ -32,23 +27,6 @@ Spotfire.initialize(async (mod: Spotfire.Mod) => {
         }
         mod.controls.errorOverlay.hide();
 
-        /**
-         * Get the hierarchy of the categorical X-axis.
-         */
-        const xHierarchy = await dataView.hierarchy("Category");
-        if (!xHierarchy) {
-            mod.controls.errorOverlay.show("Cannot find hierarchy in data view for axis 'Category'.");
-            return;
-        }
-        const xRoot = await xHierarchy.root();
-
-        if (xRoot == null) {
-            // User interaction caused the data view to expire.
-            // Don't clear the mod content here to avoid flickering.
-            return;
-        }
-
-
 
         // Get rows from dataView
         const rows = await dataView.allRows();
@@ -58,6 +36,17 @@ Spotfire.initialize(async (mod: Spotfire.Mod) => {
             return;
         }
 
+
+        // Get colors and fonts
+        var font = mod.getRenderContext().styling.general.font;
+		var backgroundColor = mod.getRenderContext().styling.general.backgroundColor;
+
+
+        // Get axis name
+        var axisCategory = dataView.categoricalAxis("Category");
+        var axisValue = dataView.continuousAxis("Value");
+
+
         // Create data values object for Vega Lite
         var datavalues = rows.map( (row, index) => ({
             a: row.categorical("Category").formattedValue(),
@@ -66,27 +55,6 @@ Spotfire.initialize(async (mod: Spotfire.Mod) => {
             rowid: row.elementId()
         }));
 
-
-        // Get colors and fonts
-        var font = mod.getRenderContext().styling.general.font;
-		var backgroundColor = mod.getRenderContext().styling.general.backgroundColor;
-
-
-        /**
-         * Print out to document
-         */
-        const container = document.querySelector("#mod-container");
-        if (!container) {
-            mod.controls.errorOverlay.show(
-                "Failed to find the DOM node with id #mod-container which should contain the visualization."
-            );
-            return;
-        }
-
-        container.textContent = `windowSize: ${windowSize.width}x${windowSize.height}\r\n`;
-        container.textContent += `should render: ${xRoot.rows().length} rows\r\n`;
-        container.textContent += `${prop.name}: ${prop.value()}\r\n`;
-        container.textContent += `Vega: ${vega.Info}`;
 
         // Assign the specification to a local variable vlSpec.
         var vlSpec : vl.TopLevelSpec = {
@@ -136,13 +104,16 @@ Spotfire.initialize(async (mod: Spotfire.Mod) => {
             }
         };
         
-        var vgSpec = vl.compile(vlSpec).spec;
 
+        // Render chart
+        var vgSpec = vl.compile(vlSpec).spec;
         var view = new vega.View(vega.parse(vgSpec), {
-            renderer:  'svg',  // renderer (canvas or svg)
-            container: '#vega-container'   // parent DOM container
+            renderer:  'svg',
+            container: '#mod-container'
         });
 
+
+        // Marking
         view.addEventListener('click', function(event, item) {
             
             var elementId = item?.datum?.rowid;
@@ -161,9 +132,9 @@ Spotfire.initialize(async (mod: Spotfire.Mod) => {
 
         view.run();
         
-        /**
-         * Signal that the mod is ready for export.
-         */
+
+        // Signal that the mod is ready for export.
         context.signalRenderComplete();
+
     }
 });
